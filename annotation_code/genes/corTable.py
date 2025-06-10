f75768793c664c2dc1a53487f2fc24dd2e6a01a0
Janek21 <jan.izquierdo@alum.esci.upf.edu>
2025-06-10 17:09:43 +0200

    f

--- annotation_code/genes/corTable.py
+++ annotation_code/genes/corTable.py
@@ -35,44 +35,61 @@ def nwShow(nw, gr_title="No module", filename=None):
         plt.show()
     plt.close()
 
+def subgraph_show(subgraph, comm_index, gene_of_interest="", folderpath=None):
+    s_pos=nx.spring_layout(subgraph, seed=42)
+    
+    nx.draw(subgraph, pos=s_pos, with_labels=False, node_color=f"C{comm_index}", node_size=100, font_size=6, edge_color="gray")
+    
+    #terms for the nodes and labels
+    labels={node: node for node in subgraph.nodes if node==gene_of_interest}
+    nx.draw_networkx_labels(subgraph, pos=s_pos, labels=labels)
+    nx.draw_networkx_nodes(subgraph, pos=s_pos, nodelist=labels, node_color="black", node_size=100)
+    
+    if folderpath!=None:
+        plt.savefig(f"{folderpath}/{comm_index}_fig.png")
+    else:
+        plt.show()
+    
+    
+import GO_comm as gc
+from collections import defaultdict
 
 def main():
-    filepath="./annotation_code/correlation_tables/brown_geneCorrelation.txt"
-    #filepath="./annotation_code/correlation_tables/turquoise_geneCorrelation.txt"
+    filepath="./correlation_tables/brown_geneCorrelation.txt"
+    #filepath="./correlation_tables/turquoise_geneCorrelation.txt"
     #Zm00001eb068950 #black
     #Zm00001eb255730 #brown
     #Zm00001eb000110 #darkred
     
     gint="Zm00001eb255730"
+    gint=""
     corTable=pd.read_table(filepath, sep="\t")
     cor_edgelist=corrCleaner(corTable, 0.6)
+    #create the graph
     nw=nwLoad(cor_edgelist)
     nwShow(nw)#, "./graphFigs/fig1.png")
 
+    #search communities
     print("Louvain:")
     louvain_comms = nx.community.louvain_communities(nw, weight="correlation", seed=42)
     
     print(f"Number of communities: {len(louvain_comms)}")
 
-    # Draw each community
+    #draw each community
     for i, comm in enumerate(louvain_comms):
         if len(comm)>1:
-            if gint in comm:
-                print(comm)
+            #if gint in comm:
+                comm=list(comm)
                 subgraph=nw.subgraph(comm)
-                s_pos=nx.spring_layout(subgraph, seed=42)
                 
-                nx.draw(subgraph, pos=s_pos, with_labels=False, node_color=f"C{i}", node_size=100, font_size=6, edge_color="gray")
-                
-                #terms for the nodes and labels
-                labels={node: node for node in subgraph.nodes if node==gint}
-                nx.draw_networkx_labels(subgraph, pos=s_pos, labels=labels)
-                nx.draw_networkx_nodes(subgraph, pos=s_pos, nodelist=labels, node_color="black", node_size=100)
-                
-                plt.show()
-                #plt.savefig(f"./graphFigs/{i}_fig.png")
+                subgraph_show(subgraph, i, gene_of_interest=gint) #, folderpath="./graphFigs")
+                #plt.close()
+
+                annot=gc.functional_analysis(comm, "zmays")
+
+                gc.genesToGo(len(comm), len(annot))
                 plt.close()
-    
+## return annotated genes and color them in plot
 
 
 if __name__ == "__main__":
